4ae9107114efdfabf358297953338d97a0c6bd73
Fred Wenzel <fwenzel@mozilla.com>
2011-01-03 17:47:15 -0800

    Renamed project to django_sha2 because it also supports SHA512 now.

--- django_sha2/models.py
+++ django_sha2/models.py
@@ -1,3 +1,3 @@
 """Make sure django.contrib.auth monkeypatching happens on load."""
-from django_sha256 import auth
+from django_sha2 import auth
 
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
-import django_sha256
+import django_sha2
 
 
 setup(
-    name='django-sha256',
-    version=django_sha256.__version__,
-    description='Enable SHA-256 password hashes in Django by default.',
+    name='django-sha2',
+    version=django_sha2.__version__,
+    description='Enable SHA-2 password hashes in Django by default.',
     long_description=open('README.md').read(),
     author='Fred Wenzel',
     author_email='fwenzel@mozilla.com',
-    url='http://github.com/fwenzel/django-sha256',
+    url='http://github.com/fwenzel/django-sha2',
     license='BSD',
-    packages=['django_sha256'],
+    packages=['django_sha2'],
     include_package_data=True,
     zip_safe=False,
     install_requires=['Django>=1.1'],
