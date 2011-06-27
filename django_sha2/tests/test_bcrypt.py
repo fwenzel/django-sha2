b71fd5498eb0dad3152d3d7e9cfd2ef85dc213ef
Mathieu Pillard <m.pillard@liberation.fr>
2011-06-27 19:53:41 +0200

    Add unicode unit tests

--- django_sha2/tests/test_bcrypt.py
+++ django_sha2/tests/test_bcrypt.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 from django import test
 from django.conf import settings
 from django.contrib.auth import authenticate
@@ -14,17 +15,22 @@ class BcryptTests(test.TestCase):
                                  password='123456')
         User.objects.create_user('jane', 'janedoe@example.com',
                                  password='abc')
+        User.objects.create_user('jude', 'jeromedoe@example.com',
+                                 password=u'abcéäêëôøà')                                 
 
     def test_bcrypt_used(self):
         """Make sure bcrypt was used as the hash."""
-        john = User.objects.get(username='john')
-        eq_(john.password[:6], 'bcrypt')
+        eq_(User.objects.get(username='john').password[:6], 'bcrypt')
+        eq_(User.objects.get(username='jane').password[:6], 'bcrypt')
+        eq_(User.objects.get(username='jude').password[:6], 'bcrypt')
 
     def test_bcrypt_auth(self):
         """Try authenticating."""
         assert authenticate(username='john', password='123456')
         assert authenticate(username='jane', password='abc')
         assert not authenticate(username='jane', password='123456')
+        assert authenticate(username='jude', password=u'abcéäêëôøà')
+        assert not authenticate(username='jude', password=u'çççbbbààà')
 
     @patch.object(settings._wrapped, 'HMAC_KEYS', dict())
     def test_nokey(self):
@@ -32,3 +38,5 @@ class BcryptTests(test.TestCase):
         assert not authenticate(username='john', password='123456')
         assert not authenticate(username='jane', password='abc')
         assert not authenticate(username='jane', password='123456')
+        assert not authenticate(username='jude', password=u'abcéäêëôøà')
+        assert not authenticate(username='jude', password=u'çççbbbààà')
